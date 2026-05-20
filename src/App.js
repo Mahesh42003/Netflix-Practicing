765ea42054c9b12935e7cc6b243961651b2342a3
Mahesh42003 <mahesh.gudipati2020@gmail.com>
2026-05-20 20:58:19 +0530

    Mahesh Changes Merged

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {ProvidingFunction} from './components/context/ReactContext'
 import StartingPage from './components/StartingPage'
 import LoginHindi from './components/LoginHindi'
+import Header from './components/Header/Header'
 const App=()=>{
   return(
     <ProvidingFunction>
@@ -9,6 +10,7 @@ const App=()=>{
      <Routes>
       <Route exact path='/in' element={<StartingPage/>}/>
       <Route exact path='/in/login-hindi' element={<LoginHindi/>}/>
+      <Route exact path='/header' element={<Header/>}/>
      </Routes>
     </BrowserRouter>
     </ProvidingFunction>
